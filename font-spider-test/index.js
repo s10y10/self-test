aecc7d02fab8b81b94883de0feb8317387fee82e
yang.song <yang.song@17zuoye.com>
2023-08-16 11:34:41 +0800

    refactor: 整理内容

--- font-spider-test/index.js
+++ font-spider-test/index.js
@@ -14,10 +14,12 @@ fontSpider
       for (let j = 0; j < font.files.length; j++) {
         let file = font.files[j];
         let fontName = file.url.replace(
-          `C:/workspace/own/self-test/font-spider-test/baseFont/`,
+          `c:/workspace/own/self-test/font-spider-test/baseFont/`,
           ''
         );
         const newPath = path.resolve(__dirname, 'output', fontName);
+        // 这里是将源字体文件拷贝一份到输出目录,并且将解析出的file.url指向拷贝后的地址
+        // 这样就可以实现在不修改源字体文件的情况下直接压缩输出目录的字体文件
         fs.copyFileSync(file.url, newPath);
         file.url = newPath;
       }
